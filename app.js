cf715327f7d660e843208adbbd158bc98c2990cd
alideveloper15 <172058104+alideveloper15@users.noreply.github.com>
2025-06-25 10:10:39 +0500

    Task Done!

---
+++ app.js
@@ -0,0 +1,24 @@
+// const num = prompt('Enter a number.')
+// if(num < 0){
+//     alert('Negative Number.')
+// }
+// else if(num >= 0){
+//     alert('Positive Number.')
+// }
+
+// const age = +prompt('Enter your age in number (for exampe: 18) to see if you are eligible for voting.')
+// if(age >=18){
+//     alert('You are eligible for voting!')
+// }
+// else{
+//     alert('You are not eligible for voting!')
+// }
+
+// const password = prompt('Enter your password.')
+// const confirmPass = prompt('Confirm your password.')
+// if(password === confirmPass){
+//     alert('Correct')
+// }
+// else{
+//     alert('Incorrect')
+// }
